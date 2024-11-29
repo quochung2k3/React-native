72ddddbe8058e2adcdbba30f430ff0c84c320055
Phạm Quốc Hưng <96472052+quochung2k3@users.noreply.github.com>
2024-11-29 15:51:08 +0700

    .

--- medical-support_nest-js/src/common/constances.ts
+++
@@ -1,9 +0,0 @@
-export const ORG_NAME = "BV Lê Văn Việt";
-
-export const MONGO_SELECT = {
-    USER: {
-        DEFAULT: '-password -activeCode -__v',
-    }
-}
-
-export const FIREBASE_IMAGE_URL = 'https://firebasestorage.googleapis.com/v0/b/medical-support-652f1.appspot.com/o'
